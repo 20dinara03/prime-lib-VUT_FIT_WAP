7d40da4cb684f50ccde5cdd10a3a7de7d37ff90f
20dinara03 <xgarip00@stud.fit.vutbr.cz>
2025-03-03 20:34:02 +0300

    Creating iterPrimes function

--- primes.mjs
+++ primes.mjs
@@ -1,34 +1,60 @@
 let cachedPrimes = [];
 
-export async function isPrime(n){
+export async function isPrime(n) {
+    if (n < 2) return false;
+    if (cachedPrimes.includes(n)) return true; 
+
+    if (n % 2 == 0){
+        return (n == 2);
+    }
+    var d = 3;
+    while (d * d <= n && n % d != 0){
+        d+=2;
+    }
+    if (d * d > n){cachedPrimes.push(n);}
+    return (d * d > n);
+}
+
+export async function getPrimes(n) {
+    let lastCachedPrime = cachedPrimes.length > 0 ? cachedPrimes[cachedPrimes.length - 1] : 1;
+
+    for (let i = lastCachedPrime + 1; i <= n; i++) {
+        if (await isPrime(i) && !cachedPrimes.includes(i)) {
+            cachedPrimes.push(i);
+        }
+    }
+    return cachedPrimes.filter(prime => prime <= n);
+}
+
+export function* iterPrimes() {
+    let index = 0;
+    let current = cachedPrimes.length > 0 ? cachedPrimes[cachedPrimes.length - 1] : 2;
+
+    while (true) {
+        if (index < cachedPrimes.length) {
+            yield cachedPrimes[index++];
+        } else {
+            while (!isPrimeSync(current)) { 
+                current++;
+            }
+            if (!cachedPrimes.includes(current)) {
+                cachedPrimes.push(current);
+            }
+            yield current;
+            current++;
+        }
+    }
+}
+
+function isPrimeSync(n) {
     if (n < 2) return false;
     if (cachedPrimes.includes(n)) return true;
 
-    for (let prime of cachedPrimes) {
-        if (prime * prime > n) break; 
-        if (n % prime === 0) return false;
-    }
-
-    let d = cachedPrimes[cachedPrimes.length - 1] + 1;
-    if (d % 2 === 0) d++; 
-
+    if (n % 2 === 0) return n === 2;
+    let d = 3;
     while (d * d <= n) {
         if (n % d === 0) return false;
         d += 2;
     }
-
-    cachedPrimes.push(n);
     return true;
-}
-
-export async function getPrimes(n){
-    let lastCachedPrime = cachedPrimes[cachedPrimes.length - 1];
-    if (n > lastCachedPrime) {
-        for (let i = lastCachedPrime + 1; i <= n; i++) {
-            if (await isPrime(i)) {
-                cachedPrimes.push(i);
-            }
-        }
-    }
-    return cachedPrimes.filter(prime => prime <= n);
 }
